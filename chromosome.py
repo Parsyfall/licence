2faf03d3d718b918c79e54a0d4c1deccdb6f2ce2
Parsyfall <dumitru.tronciu@gmail.com>
2024-04-07 21:55:51 +0300

    First commit

---
+++ chromosome.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+from math import sin, pi
+from typing import Tuple
+
+
+class Chromosome:
+    def __init__(self, point: Point) -> None:
+        self.coordinate: Point = point
+        self.fitness: float = self.eval_fitness()
+
+    @classmethod
+    def from_tuple(cls, coordinates: Tuple[float, float]) -> Chromosome:
+        return cls(Point(coordinates[0], coordinates[1]))
+
+    @classmethod
+    def from_pair(cls, X: float, Y: float) -> Chromosome:
+        return cls(Point(X, Y))
+
+    def eval_fitness(self) -> float:
+        # Rastrigin function for 2D
+        return (
+            10 * 2
+            + (self.coordinate.x**2 - 10 * sin(2 * pi * self.coordinate.x))
+            + (self.coordinate.y**2 - 10 * sin(2 * pi * self.coordinate.y))
+        )
+    
+
+class Point:
+    def __init__(self, X: float, Y: float) -> None:
+        self.x: float = X
+        self.y: float = Y
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
---
+++ main.py
@@ -0,0 +1,98 @@
+from chromosome import Chromosome
+import random as rd
+from math import dist
+
+# [ ] Implement Crowding
+# [x] Implement ftiness sharing
+
+def generate_population(size: int) -> list[Chromosome]:
+    return [
+        Chromosome.from_pair(rd.uniform(-5.12, 5.12), rd.uniform(-5.12, 5.12))
+        for _ in range(size)
+    ]
+
+
+def tournament_selection(
+    population: list[Chromosome], tournament_size: int
+) -> list[Chromosome]:
+    selected: list[Chromosome] = list()
+    while len(selected) < len(population):
+        participants = rd.sample(population, tournament_size)
+        winner = max(participants, key=lambda x: x.fitness)
+        selected.append(winner)
+    return selected
+
+
+def crossover(population: list[Chromosome]) -> list[Chromosome]:
+    #  TODO: Add a mating restriction
+
+    new_pop: list[Chromosome] = []
+    selected = tournament_selection(population, 10)
+
+    for _ in range(len(selected)):
+        index = rd.randint(0, len(selected) - 1)
+        parent1: Chromosome = population[index]
+        index = rd.randint(0, len(selected) - 1)
+        parent2: Chromosome = population[index]
+        alpha: float = rd.random()
+        child = (
+            alpha * parent1.coordinate.x + (1 - alpha) * parent2.coordinate.x,
+            alpha * parent1.coordinate.y + (1 - alpha) * parent2.coordinate.y,
+        )
+
+        new_pop.append(Chromosome.from_tuple(child))
+
+    return new_pop
+
+
+def mutation(population: list[Chromosome], probability: float) -> None:
+    # Bounds for Rastrigin function
+    upper_bound = 5.12
+    lower_bound = -5.12
+
+    for gene in population:
+        if rd.random() < probability:
+            # Mutate X
+            if rd.random() < 0.5:
+                gene.coordinate.x -= rd.random() * (gene.coordinate.x - lower_bound)
+            else:
+                gene.coordinate.x += rd.random() * (upper_bound - gene.coordinate.x)
+            # Mutate Y
+            if rd.random() < 0.5:
+                gene.coordinate.y -= rd.random() * (gene.coordinate.y - lower_bound)
+            else:
+                gene.coordinate.y += rd.random() * (upper_bound - gene.coordinate.y)
+
+            # Reevaluate fitness of mutants
+            gene.eval_fitness()
+
+
+def sharing(distance: float, sigma: float, alpha: float = 1) -> float:
+    """
+    Sharing function of my GA that decides if 2 individuals are close enought to share fitness
+    based on distance between them and the threshold of dissimilarity (sigma)
+    """
+    # If distance between 2 individuals is less than sharing distance (sigma) -> they share fitness
+    return 1 - (distance / sigma) ** alpha if distance < sigma else 0
+
+
+def fitness_sharing(
+    population: list[Chromosome], target_individual: Chromosome, radius: float
+) -> None:
+    # TODO: Try and use clustering algorithms to divite population in smaller groups
+    niche_count = 0
+    for indiv in population:
+        niche_count += sharing(
+            dist(indiv.coordinate, target_individual.coordinate), len(population) / 3
+        )
+
+    target_individual.fitness = target_individual.fitness / niche_count
+
+
+def run_evolution(generations: int):
+    population = generate_population(100)
+    current_generation = 0
+
+    while current_generation < generations:
+        pass
+    
